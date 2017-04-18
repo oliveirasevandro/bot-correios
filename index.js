ff22828952ac0f4e46d1fcba14d832999d47af01
Evandro Oliveira <evandro.oliveira@matera.com>
2017-04-18 14:12:03 -0300

    Fixed server

--- index.js
+++ index.js
@@ -27,3 +27,7 @@ app.get('/webhook', function (req, res) {
    res.send('Error, wrong token');
 
 });
+
+app.listen(app.get('port'), function () {
+    console.log('running on port ', app.get('port'));
+});
